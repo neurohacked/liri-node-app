27e09621d233f6b3d438bd5cb0eef973331117b5
neurohacked <patrick@neurohacked.com>
2016-09-05 15:16:51 -0700

    Add default movie
    Added Mr. Nobody as default movie.

--- navi.js
+++ navi.js
@@ -8,45 +8,48 @@ var nodeArgs = process.argv;
 /**
  * OMDB Functionality
  **/
- if (process.argv[2] === "movie-this") {
-var movieName = "";
-
-for (var i = 3; i < nodeArgs.length; i++) {
-
-    if (i > 3 && i < nodeArgs.length) {
-
-        movieName = movieName + "+" + nodeArgs[i];
-
+if (process.argv[2] === "movie-this") {
+    if (process.argv[3]) {
+        var movieName = "";
+        for (var i = 3; i < nodeArgs.length; i++) {
+            if (i > 3 && i < nodeArgs.length) {
+                movieName = movieName + "+" + nodeArgs[i];
+            } else {
+                movieName = movieName + nodeArgs[i];
+            }
+        }
     } else {
-
-        movieName = movieName + nodeArgs[i];
+        var movieName = "Mr.+Nobody";
     }
-}
 
-var queryUrl = 'http://www.omdbapi.com/?t=' + movieName + '&y=&plot=short&tomatoes=true&r=json';
+    var queryUrl = 'http://www.omdbapi.com/?t=' + movieName + '&y=&plot=short&tomatoes=true&r=json';
 
-request(queryUrl, function(error, response, body) {
-    if (!error && response.statusCode == 200) {
-        console.log('');
-        console.log('================================');
-        console.log("Movie: " + JSON.parse(body).Title);
-        console.log('================================');
-        console.log('');
-        console.log("Plot: " + JSON.parse(body).Plot);
-        console.log('');
-		console.log("Year Released: " + JSON.parse(body).Year);
-		console.log("Country: " + JSON.parse(body).Country);
-		console.log("Language: " + JSON.parse(body).Language);
-		console.log("Actors: " + JSON.parse(body).Actors);
-        console.log('');
-		console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-		console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
-		console.log("Rotten Tomatoes URL: " + JSON.parse(body).tomatoURL);
-        console.log('');
-        console.log('================================');
-        console.log('');
-    }
-});
+    request(queryUrl, function(error, response, body) {
+        if (error) {
+            console.log('Error occurred: ' + error);
+            return;
+        }
+        if (!error && response.statusCode == 200) {
+            console.log('');
+            console.log('================================');
+            console.log("Movie: " + JSON.parse(body).Title);
+            console.log('================================');
+            console.log('');
+            console.log("Plot: " + JSON.parse(body).Plot);
+            console.log('');
+            console.log("Year Released: " + JSON.parse(body).Year);
+            console.log("Country: " + JSON.parse(body).Country);
+            console.log("Language: " + JSON.parse(body).Language);
+            console.log("Actors: " + JSON.parse(body).Actors);
+            console.log('');
+            console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+            console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
+            console.log("Rotten Tomatoes URL: " + JSON.parse(body).tomatoURL);
+            console.log('');
+            console.log('================================');
+            console.log('');
+        }
+    });
 
 }
 
